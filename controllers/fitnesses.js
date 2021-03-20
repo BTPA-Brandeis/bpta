b8c8033178273e5cb6521188d9a634907cc72c7e
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-20 16:38:23 -0400

    add fitnesses in controller

--- controllers/fitnesses.js
+++ controllers/fitnesses.js
@@ -6,3 +6,21 @@ export const getAllFitnesses = async (request, response) => {
   return response.send(fitnesses)
 }
 
+export const saveNewFitnesses = async (request, response) => {
+  try {
+    const { userID, pushUps, sitUps, boxJumps, kilometersRan, waterIntakeOz } = request.body
+
+    if (!userID || !pushUps || !sitUps || !boxJumps || !kilometersRan || !waterIntakeOz) {
+      return response
+        .status(400)
+        .send('The following fields are required: userID, pushUps, sitUps, boxJumps, kilometersRan, waterIntakeOz')
+    }
+
+    const newFitnesses = await models.Fitnesses.create({ userID, pushUps, sitUps, boxJumps, kilometersRan, waterIntakeOz })
+
+    return response.status(201).send(newFitnesses)
+  } catch (error) {
+    return response.status(500).send('Unable to save new fitnesses, please try again')
+  }
+}
+
