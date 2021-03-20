b1bd0e52b282ebac08da8bfac1e5c54b0e1c312d
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-20 10:54:12 -0400

    initial dataload set up however no migration allowed while integers null not allowed

--- migrations/20210320142953-initial-data-load.js
+++ migrations/20210320142953-initial-data-load.js
@@ -8,6 +8,25 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.bulkInsert('users', [
+      { userFirstName: '', userLastName: '', password: '', accessLevel: '', email: '', DOB: '', profileImage: '', securityQuestionOne: '', securityQuestionTwo: '', locationZone: '' },
+    ]),
+
+      await queryInterface.bulkInsert('fitness', [
+        { userID: '', pushUps: '', sitUps: '', boxJumps: '', kilometersRan: '', waterIntakeOz: '' }
+      ]),
+
+      await queryInterface.bulkInsert('usersFitnessLinkings', [
+        { userID: '', fitnessID: '' }
+      ]),
+
+      await queryInterface.bulkInsert('posts', [
+        { accessLevelReq: '', body: '', userID: '' }
+      ])
+
+    return queryInterface.bulkInsert('resources', [
+      { url: '', description: '', subject: '' }
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -17,5 +36,14 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.bulkDelete('resources')
+
+    await queryInterface.bulkDelete('posts')
+
+    await queryInterface.bulkDelete('usuersFitnessLinkings')
+
+    await queryInterface.bulkDelete('fitness')
+
+    return queryInterface.bulkDelete('users')
   }
 };
