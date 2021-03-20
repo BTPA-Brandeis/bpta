b61f0c2dbe0350818e2d1cbcc211be8ea35e9197
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-20 11:35:23 -0400

    function import and get add to server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+// import bodyParser from 'body-parser'
 import path from 'path'
+import { getAllUsers, getUserByUserLastName } from './controllers/users'
+
 const app = express()
 app.use(express.static('public'))
+
+app.get('/api/users', getAllUsers)
+app.get('/api/users/:getUsersByUsersLastName', getUserByUserLastName)
+
 app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+
 app.listen(1337, () => {
   console.log('Listening on port 1337...')
 })
