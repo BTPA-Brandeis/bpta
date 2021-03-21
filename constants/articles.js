73c9a4e2f842f93d04515ff55843418fdd409bfb
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-21 11:04:25 -0400

    articles update with email

--- constants/articles.js
+++ constants/articles.js
@@ -2,34 +2,34 @@ export default [
   {
     title: 'Darryl Owens, Lead instructor and co-founder of the BTPA. He feels blessed to be able to spend his summers this way. Having grown up in a crime-affected single parent household in inner-city Boston, he refuses to move up and out and has dedicated his life to serving his people. The Teen Academy started as his dream and now that he realized it, it is his passion.',
     image: 'https://bostonteenpoliceacademy.org/wp-content/uploads/2019/04/HH-Darryl_Owens.jpg',
-    cta: 'Read More', 
+    cta: 'Reach Out Darryl-Owens@pd.boston.gov', 
     horizontal: true
   },
   {
     title: 'Lylana Jones-Henderson, A police officer with the Boston Police Department for 13 years and has served in the Jamaica Plain and Roxbury neighborhoods',
     image: 'https://bostonteenpoliceacademy.org/wp-content/uploads/2019/04/094734_Jones-Henderson_-Lylana_0009.jpg',
-    cta: 'Read More'
+    cta: 'Reach Out LylanaJones-Henderson@pd.boston.gov'
   },
   {
     title: 'Marie Miller, Introduced to the Teen Police Academy by Sgt. William Baxter when she was assigned to the Community Service Office. She immediately fell in love with the program and has been a part for the past 9 years',
     image: 'https://bostonteenpoliceacademy.org/wp-content/uploads/2019/04/Officer-Marie-Miller.jpg',
-    cta: 'Read More' 
+    cta: 'Reach Out Marie-Miller@pd.boston.gov' 
   },
   {
     title: 'Cynthia Brewington, Working with the youth and witnessing the positive change within them leaves your heart booming with pride. We may not have a positive impact on every single student, but the ones we do become like our own children',
     image: '"https://bostonteenpoliceacademy.org/wp-content/uploads/2019/04/Officer-Cynthia-Brewington.jpg',
-    cta: 'Read More' 
+    cta: 'Reach Out Cynthia-Brewington@pd.boston.gov' 
   },
   {
     title: 'Ydritzabel “Angie” Oller, She has been working with the Teen Academy for three years. Her ability to relate to the teen is mazing her wit and charm go hand in hand',
     image: 'https://bostonteenpoliceacademy.org/wp-content/uploads/2019/04/Officer-Ydritzabel-%E2%80%9CAngie%E2%80%9D-Oller.jpg',
-    cta: 'Read More', 
+    cta: 'Reach Out Ydritzabel-Angie-Oller@pd.boston.gov', 
     horizontal: true
   },
   {
     title: 'Jean-Paul Limontas (J.P.), I became part of BPD Teen Police Academy in 2018. TPA allows me the opportunity to share my passion for wellness as well as connect with youth in a meaningful and authentic way',
     image: '"https://bostonteenpoliceacademy.org/wp-content/uploads/2019/04/Officer-Jean-Paul-Limontas-J.P..jpg',
-    cta: 'Read More', 
+    cta: 'Reach Out Jean-Paul-Limontas@pd.boston.gov', 
     horizontal: true
   },
 ];
