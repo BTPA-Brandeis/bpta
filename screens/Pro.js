65a4153f6164e12f7bde5b3f95b783d7e2acb4c1
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-03-21 02:50:25 -0400

    Update Login Page

--- screens/Pro.js
+++ screens/Pro.js
@@ -1,112 +1,262 @@
 import React from "react";
 import {
-  ImageBackground,
-  Image,
   StyleSheet,
-  StatusBar,
+  ImageBackground,
   Dimensions,
-  Platform
+  StatusBar,
+  TouchableWithoutFeedback,
+  Keyboard
 } from "react-native";
-import { Block, Button, Text, theme } from "galio-framework";
+import { Block, Checkbox, Text } from "galio-framework";
 
-const { height, width } = Dimensions.get("screen");
-import { Images, argonTheme } from "../constants/";
-import { HeaderHeight } from "../constants/utils";
+import { Button, Icon, Input } from "../components";
+import { Images, argonTheme } from "../constants";
 
-export default class Pro extends React.Component {
+const { width, height } = Dimensions.get("screen");
+
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
+
+class Register extends React.Component {
   render() {
     const { navigation } = this.props;
-
     return (
-      <Block flex style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Block flex>
+      <DismissKeyboard>
+        <Block flex middle>
+          <StatusBar hidden />
           <ImageBackground
-            source={Images.Pro}
-            style={{ flex: 1, height: height, width, zIndex: 1 }}
-          />
-          <Block space="between" style={styles.padded}>
-            <Block>
-              <Block>
-                <Block>
-                  <Text style={{ fontFamily: 'open-sans-regular' }} color="white" size={60}>
-                   Sign In
-                  </Text>
-                </Block>
-                <Block>
-                  <Text style={{ fontFamily: 'open-sans-regular' }} color="white" size={60}>
-                    Design
-                  </Text>
-                </Block>
-                <Block row>
-                  <Text style={{ fontFamily: 'open-sans-regular' }} color="white" size={60}>
-                    System
-                  </Text>
-                  <Block middle style={styles.pro}>
-                    <Text style={{ fontFamily: 'open-sans-bold' }} size={16} color="white">
-                      PRO
-                    </Text>
+            source={Images.RegisterBackground}
+            style={{ width, height, zIndex: 1 }}
+          >
+            <Block flex middle>
+              <Block style={styles.registerContainer}>
+                <Block flex space="between">
+                  <Block flex={0.2} middle style={styles.socialConnect}>
+                    <Block flex={0.6} middle>
+                      <Text color="#8898AA" size={12}>
+                        Sign up with
+                      </Text>
+                    </Block>
+                    <Block flex={0.4} row style={{ marginBottom: 18 }}>
+                      <Button
+                        style={{ ...styles.socialButtons, marginRight: 30 }}
+                      >
+                        <Block row>
+                          <Icon
+                            name="logo-google"
+                            family="Ionicon"
+                            size={14}
+                            color={"black"}
+                            style={{ marginTop: 2, marginRight: 5 }}
+                          />
+                          <Text style={styles.socialTextButtons}>GOOGLE</Text>
+                        </Block>
+                      </Button>
+                      <Button style={styles.socialButtons}>
+                        <Block row>
+                          <Icon
+                            name="apple"
+                            family="font-awesome"
+                            size={14}
+                            color={"black"}
+                            style={{ marginTop: 2, marginRight: 5 }}
+                          />
+                          <Text style={styles.socialTextButtons}>APPLE</Text>
+                        </Block>
+                      </Button>
+                    </Block>
+                  </Block>
+                  <Block flex={0.8} middle space="between">
+                    <Block flex={0.2} middle>
+                      <Text
+                        style={{
+                          fontFamily: "open-sans-regular",
+                          textAlign: "center"
+                        }}
+                        color="#8898AA"
+                        size={12}
+                      >
+                        Or sign up the classic way
+                      </Text>
+                    </Block>
+                    <Block center flex={0.9}>
+                      <Block flex space="between">
+                        <Block>
+                          <Block
+                            width={width * 0.8}
+                            style={{ marginBottom: 5 }}
+                          >
+                            <Input
+                              borderless
+                              placeholder="First Name"
+                              iconContent={
+                                <Icon
+                                  size={16}
+                                  color="#ADB5BD"
+                                  name="hat-3"
+                                  family="ArgonExtra"
+                                  style={styles.inputIcons}
+                                />
+                              }
+                            />
+                          </Block>
+                          <Block
+                            width={width * 0.8}
+                            style={{ marginBottom: 5 }}
+                          >
+                            <Input
+                              borderless
+                              placeholder="Last Name"
+                              iconContent={
+                                <Icon
+                                  size={16}
+                                  color="#ADB5BD"
+                                  name="ic_mail_24px"
+                                  family="ArgonExtra"
+                                  style={styles.inputIcons}
+                                />
+                              }
+                            />
+                          </Block>
+                          <Block width={width * 0.8}>
+                            <Input
+                              password
+                              borderless
+                              placeholder="Email"
+                              iconContent={
+                                <Icon
+                                  size={16}
+                                  color="#ADB5BD"
+                                  name="padlock-unlocked"
+                                  family="ArgonExtra"
+                                  style={styles.inputIcons}
+                                />
+                              }
+                            />
+                            <Block row style={styles.passwordCheck}>
+                              <Text
+                                style={{ fontFamily: "open-sans-regular" }}
+                                size={12}
+                                color={argonTheme.COLORS.MUTED}
+                              >
+                                password strength:
+                              </Text>
+                              <Text
+                                style={{ fontFamily: "open-sans-bold" }}
+                                size={12}
+                                color={argonTheme.COLORS.SUCCESS}
+                              >
+                                {" "}
+                                strong
+                              </Text>
+                            </Block>
+                          </Block>
+                          <Block row width={width * 0.75}>
+                            <Checkbox
+                              checkboxStyle={{
+                                borderWidth: 1
+                              }}
+                              color={argonTheme.COLORS.PRIMARY}
+                              labelStyle={{
+                                color: argonTheme.COLORS.TEXT,
+                                fontFamily: "open-sans-regular"
+                              }}
+                              label="I agree with the"
+                            />
+                            <Button
+                              style={{ width: 100 }}
+                              color="transparent"
+                              textStyle={{
+                                color: argonTheme.COLORS.PRIMARY,
+                                fontSize: 14,
+                                fontFamily: "open-sans-regular",
+                                marginRight: 5
+                              }}
+                            >
+                              Privacy Policy
+                            </Button>
+                          </Block>
+                        </Block>
+                        <Block center>
+                          <Button color="primary" style={styles.createButton}>
+                            <Text
+                              style={{ fontFamily: "open-sans-bold" }}
+                              size={14}
+                              color={argonTheme.COLORS.WHITE}
+                              onPress={() => navigation.navigate("App")}
+                            >
+                              CREATE ACCOUNT
+                            </Text>
+                          </Button>
+                        </Block>
+                      </Block>
+                    </Block>
                   </Block>
                 </Block>
               </Block>
-              <Text
-                size={20}
-                color="rgba(255,255,255,0.6)"
-                style={{ marginTop: 35, fontFamily: 'open-sans-regular' }}
-              >
-                Take advantage of all the features and screens made upon Galio
-                Design System, coded on React Native for both.
-              </Text>
-              <Button
-                shadowless
-                style={styles.button}
-                color={argonTheme.COLORS.INFO}
-                onPress={() => navigation.navigate("App")}
-              >
-                <Text style={{ fontFamily: 'open-sans-bold', fontSize: 14 }} color={theme.COLORS.WHITE}>
-                  GET STARTED
-                </Text>
-              </Button>
             </Block>
-          </Block>
+          </ImageBackground>
         </Block>
-      </Block>
+      </DismissKeyboard>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: theme.COLORS.BLACK,
-    marginTop: Platform.OS === "android" ? -HeaderHeight : 0
-  },
-  padded: {
-    paddingHorizontal: theme.SIZES.BASE * 2,
-    zIndex: 3,
-    position: "absolute",
-    bottom:
-      Platform.OS === "android" ? theme.SIZES.BASE * 2 : theme.SIZES.BASE * 3
-  },
-  button: {
-    width: width - theme.SIZES.BASE * 4,
-    height: theme.SIZES.BASE * 3,
-    shadowRadius: 0,
-    shadowOpacity: 0
-  },
-  pro: {
-    backgroundColor: argonTheme.COLORS.INFO,
-    paddingHorizontal: 8,
-    marginLeft: 3,
+  registerContainer: {
+    width: width * 0.9,
+    height: height < 812 ? height * 0.9 : height * 0.8,
+    backgroundColor: "#F4F5F7",
     borderRadius: 4,
-    height: 22,
-    marginTop: 15
+    shadowColor: argonTheme.COLORS.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1,
+    overflow: "hidden"
   },
-  gradient: {
-    zIndex: 1,
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 66
+  socialConnect: {
+    backgroundColor: argonTheme.COLORS.WHITE,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "rgba(136, 152, 170, 0.3)"
+  },
+  socialButtons: {
+    width: 120,
+    height: 40,
+    backgroundColor: "#fff",
+    shadowColor: argonTheme.COLORS.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1
+  },
+  socialTextButtons: {
+    color: argonTheme.COLORS.PRIMARY,
+    fontWeight: "800",
+    fontSize: 14
+  },
+  inputIcons: {
+    marginRight: 12
+  },
+  passwordCheck: {
+    paddingLeft: 2,
+    paddingTop: 6,
+    paddingBottom: 15
+  },
+  createButton: {
+    width: width * 0.5,
+    marginTop: 25,
+    marginBottom: 40
   }
 });
+
+export default Register;
